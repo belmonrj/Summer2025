b62753d21fec48cb6477ce72c44e1e626700069c
Ron Belmont <belmonrj@proton.me>
2025-07-22 10:36:21 -0400

    Added Jamie's cacl6 function and my updated version of it

--- FlowPackage/start_simple.C
+++ FlowPackage/start_simple.C
@@ -212,6 +212,121 @@ double calc4event(const std::array<TComplex,max_harmonic>& allQ, int harmonic)
   return numerator/denominator;
 }
 
+// <cos(n(phi1+phi2+phi3-phi4-phi5-phi6))>
+double calc6_event_jamie(TComplex& qn, TComplex& q2n, TComplex& q3n, float M)
+{
+
+  if ( M < 6 ) return -9999;
+
+  // TComplex qn, q2n, q3n;
+  // qn = TComplex(Q2x,Q2y);
+  // q2n = TComplex(Q4x,Q4y);
+  // q3n = TComplex(Q6x,Q6y);
+
+  TComplex temp1;
+
+  // first term
+  // |Qn|^6 + 9*|Q2n|^2|Qn|^2 - 6 x Re[Q2n x Qn x Qn* x Qn* x Qn*] / (Mx(M-1)x(M-2)x(M-3)x(M-4)x(M-5)
+  double term1a = TMath::Power((qn*TComplex::Conjugate(qn)),3);
+  double term1b = 9.0 * q2n*TComplex::Conjugate(q2n) * qn*TComplex::Conjugate(qn);
+  temp1 = q2n * qn * TComplex::Conjugate(qn) * TComplex::Conjugate(qn) * TComplex::Conjugate(qn);
+  double term1c = -6.0 * temp1.Re();
+  double term1 = (term1a+term1b+term1c)/(M*(M-1)*(M-2)*(M-3)*(M-4)*(M-5));
+
+  // second term
+  // 4 * [Re[Q3nQn*Qn*Qn*] - 3 Re[Q3nQ2n*Qn*]] / (M(M-1)(M-2)(M-3)(M-4)(M-5)
+  temp1 = q3n * TComplex::Conjugate(qn) * TComplex::Conjugate(qn) * TComplex::Conjugate(qn);
+  double term2a = temp1.Re();
+  temp1 = q3n * TComplex::Conjugate(q2n) * TComplex::Conjugate(qn);
+  double term2b = -3.0 * temp1.Re();
+  double term2 = 4.0 * (term2a+term2b)/(M*(M-1)*(M-2)*(M-3)*(M-4)*(M-5));
+
+  // third term
+  // +2 * (9*(M-4)*Re[Q2nQn*qn*] + 2 |Q3n|^2) / ((M(M-1)(M-2)(M-3)(M-4)(M-5))
+  temp1 = q2n*TComplex::Conjugate(qn)*TComplex::Conjugate(qn);
+  double term3a = 9.0*(M-4)*temp1.Re();
+  double term3b = 2.0*q3n*TComplex::Conjugate(q3n);
+  double term3 = 2.0 * (term3a + term3b) / (M*(M-1)*(M-2)*(M-3)*(M-4)*(M-5));
+
+  // fourth term
+  //double term4 = -9.0 * (TMath::Power(qn*TComplex::Conjugate(qn),2)+q2n*TComplex::Conjugate(q2n)) / (M*(M-1)*(M-2)*(M-3)*(M-5));
+  double term4 = -9.0 * (TMath::Power(qn*TComplex::Conjugate(qn),2)+q2n*TComplex::Conjugate(q2n)) ;
+  term4 /= (M*(M-1)*(M-2)*(M-3)*(M-5));
+
+  // fifth term
+  //double term5 = 18.0 * qn*TComplex::Conjugate(qn) / (M*(M-1)*(M-3)*(M-4));
+  double term5 = 18.0 * qn*TComplex::Conjugate(qn) ;
+  term5 /=  (M*(M-1)*(M-3)*(M-4));
+
+  // sixth term
+  double term6 = -6.0/((M-1)*(M-2)*(M-3));
+
+  // cos(n(phi1+phi2+phi3-phi4-phi5-phi6))
+  double six = term1 + term2 + term3 + term4 + term5 + term6;
+
+  return six;
+
+}
+
+// <cos(n(phi1+phi2+phi3-phi4-phi5-phi6))>
+double calc6event(const std::array<TComplex,max_harmonic>& allQ, int harmonic)
+{
+
+  double M = allQ[0].Re();
+  if ( M < 6 ) return -9999;
+  // ---
+  TComplex Q = allQ[harmonic];
+  TComplex Q2 = allQ[2*harmonic];
+  TComplex Q3 = allQ[3*harmonic];
+  TComplex Qstar = TComplex::Conjugate(allQ[harmonic]);
+  TComplex Q2star = TComplex::Conjugate(allQ[2*harmonic]);
+  TComplex Q3star = TComplex::Conjugate(allQ[3*harmonic]);
+
+  TComplex temp1;
+
+  // first term
+  // |Qn|^6 + 9*|Q2n|^2|Qn|^2 - 6 x Re[Q2n x Qn x Qn* x Qn* x Qn*] / (Mx(M-1)x(M-2)x(M-3)x(M-4)x(M-5)
+  double term1a = TMath::Power((Q*Qstar),3);
+  double term1b = 9.0 * Q2*Q2star * Q*Qstar;
+  temp1 = Q2 * Q * Qstar * Qstar * Qstar;
+  double term1c = -6.0 * temp1.Re();
+  double term1 = (term1a+term1b+term1c)/(M*(M-1)*(M-2)*(M-3)*(M-4)*(M-5));
+
+  // second term
+  // 4 * [Re[Q3nQn*Qn*Qn*] - 3 Re[Q3nQ2n*Qn*]] / (M(M-1)(M-2)(M-3)(M-4)(M-5)
+  temp1 = Q3 * Qstar * Qstar * Qstar;
+  double term2a = temp1.Re();
+  temp1 = Q3 * Q2star * Qstar;
+  double term2b = -3.0 * temp1.Re();
+  double term2 = 4.0 * (term2a+term2b)/(M*(M-1)*(M-2)*(M-3)*(M-4)*(M-5));
+
+  // third term
+  // +2 * (9*(M-4)*Re[Q2nQn*qn*] + 2 |Q3n|^2) / ((M(M-1)(M-2)(M-3)(M-4)(M-5))
+  temp1 = Q2*Qstar*Qstar;
+  double term3a = 9.0*(M-4)*temp1.Re();
+  double term3b = 2.0*Q3*Q3star;
+  double term3 = 2.0 * (term3a + term3b) / (M*(M-1)*(M-2)*(M-3)*(M-4)*(M-5));
+
+  // fourth term
+  //double term4 = -9.0 * (TMath::Power(Q*Qstar,2)+Q2*Q2star) / (M*(M-1)*(M-2)*(M-3)*(M-5));
+  double term4 = -9.0 * (TMath::Power(Q*Qstar,2)+Q2*Q2star) ;
+  term4 /= (M*(M-1)*(M-2)*(M-3)*(M-5));
+
+  // fifth term
+  //double term5 = 18.0 * Q*Qstar / (M*(M-1)*(M-3)*(M-4));
+  double term5 = 18.0 * Q*Qstar ;
+  term5 /=  (M*(M-1)*(M-3)*(M-4));
+
+  // sixth term
+  double term6 = -6.0/((M-1)*(M-2)*(M-3));
+
+  // cos(n(phi1+phi2+phi3-phi4-phi5-phi6))
+  double six = term1 + term2 + term3 + term4 + term5 + term6;
+
+  return six;
+
+}
+
 // --- from generic forumulas ----------------------------------------------------
 
 TComplex recQ(int n, int p)
@@ -329,7 +444,7 @@ void start_simple()
   std::cout << fake_weighted_flow_all[2][3] << std::endl;
   std::cout << fake_weighted_flow_all[2][4] << std::endl;
 
-  return;
+  //return;
 
   for ( int i = 0; i < max_harmonic; ++i )
     {
@@ -407,9 +522,11 @@ void start_simple()
   std::cout << "Now need denominator..." << std::endl;
   double denominator6 = Recursion(6,test6den).Re();
   double super_smart_calc6 = numerator6/denominator6;
-  //double smart_calc6 = calc6event(fake_flow_all,2);
+  double jamie_smart_calc6 = calc6_event_jamie(fake_flow_all[2],fake_flow_all[4],fake_flow_all[6],fake_flow_all[0]);
+  double smart_calc6 = calc6event(fake_flow_all,2);
 
-  //std::cout << "Flow vector based calculation of <6> (harmonic=2) is " << smart_calc6 << std::endl;
+  std::cout << "Jamie's flow vector based calculation of <6> (harmonic=2) is " << jamie_smart_calc6 << std::endl;
+  std::cout << "Flow vector based calculation of <6> (harmonic=2) is " << smart_calc6 << std::endl;
   std::cout << "Recursion based calculation of <6> (harmonic=2) is " << super_smart_calc6 << std::endl;
 
 }
